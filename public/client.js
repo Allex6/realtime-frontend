3ea98ae57cc02eca24cb6c241e7c422e6faa9744
AleexHunteer <spfc.alex4@gmail.com>
2020-03-05 17:14:26 -0300

    up

--- public/client.js
+++ public/client.js
@@ -2,26 +2,24 @@ window.onload = ()=>{
 
 	var socket = io('http://localhost:9000');
 
-	//console.log(window.document.documentElement);
-
 	socket.emit('document-loaded', window.document.location.href);
 
 	socket.on('file-change', (data)=>{
 
 		switch (data.ext) {
 
+			case '.ejs':
 			case '.html':
 				document.documentElement.innerHTML = data.data;
 				break;
 
 			case '.css':
-				
-				let link = document.createElement("link");
-				link.rel = "stylesheet";
-				link.type = "text/css";
-				link.href = data.file;
-				document.querySelector("head").appendChild(link);
-				document.querySelector(`[href='${data.fileHref}']`).remove();
+
+				if (document.querySelector(`[href='${data.fileHref}']`)) {
+
+					document.querySelector(`[href='${data.fileHref}']`).href = data.fileHref;
+
+				}
 
 				break;
 			
